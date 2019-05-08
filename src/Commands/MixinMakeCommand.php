6f6f01d6a2d55b163b9b93cfcd893951a787af74
laminblur <laminblur@gmail.com>
2019-05-08 11:55:31 +0800

    Update MixinMakeCommand.php

--- src/Commands/MixinMakeCommand.php
+++ src/Commands/MixinMakeCommand.php
@@ -12,22 +12,19 @@ class MixinMakeCommand extends GeneratorCommand
      *
      * @var string
      */
-    protected $name = 'make:vue-mixin {--empty}';
-
+    protected $name = 'make:vue-mixin';
     /**
      * The console command description.
      *
      * @var string
      */
     protected $description = 'Create a new Vue mixin file';
-
     /**
      * The type of class being generated.
      *
      * @var string
      */
     protected $type = 'Vue Mixin';
-
     /**
      * Get the stub file for the generator.
      *
@@ -35,11 +32,8 @@ class MixinMakeCommand extends GeneratorCommand
      */
     protected function getStub()
     {
-         $fileName = $this->option('empty') ? 'EmptyMixin' : 'mixin';
-    
-        return config('vue-generators.mixin_stub', __DIR__.'/../stubs/'.$fileName.'.stub');
+        return config('vue-generators.mixin_stub', __DIR__.'/../stubs/mixin.stub');
     }
-
     /**
      * Build the class with the given name.
      *
@@ -51,7 +45,6 @@ class MixinMakeCommand extends GeneratorCommand
     {
         return $this->files->get($this->getStub());
     }
-
     /**
      * Get the destination class path.
      *
